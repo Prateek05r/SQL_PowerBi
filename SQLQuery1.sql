c65eeb51be3b7c21f367e99b13f2e1789abd9a58
Prateek05r <136294863+Prateek05r@users.noreply.github.com>
2023-06-23 12:10:10 +0530

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,24 @@
+SELECT 
+  [DateKey], 
+  [FullDateAlternateKey]AS Date, 
+  --,[DayNumberOfWeek]
+  [EnglishDayNameOfWeek]AS Day, 
+  --,[SpanishDayNameOfWeek]
+  --,[FrenchDayNameOfWeek]
+  --,[DayNumberOfMonth]
+  --,[DayNumberOfYear]
+  [WeekNumberOfYear]AS weekno, 
+  [EnglishMonthName]As Month, 
+  LEFT([EnglishMonthName],3)AS Monthshort,
+  --,[SpanishMonthName]
+  --,[FrenchMonthName]
+  [MonthNumberOfYear]AS monthno, 
+  [CalendarQuarter]AS Quarter, 
+  [CalendarYear] AS Year,
+  --,[CalendarSemester]
+  --,[FiscalQuarter]
+  --,[FiscalYear]
+ -[FiscalSemester]
+FROM
+  [AdventureWorksDW2022].[dbo].[DimDate]
+  where CalendarYear>=2019
